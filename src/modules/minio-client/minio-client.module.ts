427ab145cddf2107205ba8ce3e58d8e02a882b99
Artem Oleinikov <38537243+OleinikovArtem@users.noreply.github.com>
2024-05-08 12:33:20 +0300

    Feature/docker update (#1)
    Update docker config

--- src/modules/minio-client/minio-client.module.ts
+++ src/modules/minio-client/minio-client.module.ts
@@ -3,15 +3,14 @@ import { MinioClientService } from './minio-client.service';
 import { MinioModule } from 'nestjs-minio-client';
 import { minioConfig } from 'src/config/minio.config';
 
-const { minio_endpoint, minio_secret_key, minio_access_key, minio_port } =
-  minioConfig();
+const { minio_endpoint, minio_secret_key, minio_access_key } = minioConfig();
 
 @Module({
   imports: [
     MinioModule.register({
       endPoint: minio_endpoint,
-      port: minio_port,
-      useSSL: false,
+      port: 443,
+      useSSL: true,
       accessKey: minio_access_key,
       secretKey: minio_secret_key,
     }),
